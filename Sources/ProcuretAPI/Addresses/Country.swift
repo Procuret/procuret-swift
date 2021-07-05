ff3bfc4373ad9ecad51c172d807aca433710b6f7
Kayla <kayla.h@procuret.com>
2021-07-05 19:19:35 -0400

    country struct for swift api #1

---
+++ Sources/ProcuretAPI/Addresses/Country.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/5/21.
+//
+
+import Foundation
+
+
+public struct Country: Codable {
+    
+    let id: Int
+    let name: String
+    let abbreviation: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "country_id"
+        case name = "name"
+        case abbreviation = "abbreviation"
+    }
+
+}
