89ee497afa88b86a11ae3781b05306113586d708
Kayla <kayla.h@procuret.com>
2021-07-21 15:44:00 -0400

    basic struct for entity and entityIdentifier #6

---
+++ Sources/ProcuretAPI/Entity/Entity.swift
@@ -0,0 +1,34 @@
+//
+//  Entity.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/21/21.
+//
+
+import Foundation
+
+public struct Entity: Codable {
+    let publicId: Int
+    let publicIdShort: Int
+    let entityIdentifiers: Array<EntityIdentifier>
+    let legalEntityName: String
+    let tradingName: String?
+    let phoneNumber: String?
+    let businessAddress: String
+    let entityType: String?
+    let created: String?
+    let disposition: Disposition
+    
+    public enum CodingKeys: String, CodingKey {
+        case publicId = "public_id"
+        case publicIdShort = "public_id_short"
+        case entityIdentifiers = "entity_identifiers"
+        case legalEntityName = "legal_entity_name"
+        case tradingName = "trading_name"
+        case phoneNumber = "phone_number"
+        case businessAddress = "business_address"
+        case entityType = "entity_type"
+        case created
+        case disposition
+    }
+}
---
+++ Sources/ProcuretAPI/Entity/Identifier.swift
@@ -0,0 +1,18 @@
+//
+//  Identifier.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/21/21.
+//
+
+import Foundation
+
+public struct EntityIdentifier: Codable {
+    let idType: String
+    let idString: String
+    
+    public enum CodingKeys: String, CodingKey {
+        case idType = "id_type"
+        case idString = "id"
+    }
+}
