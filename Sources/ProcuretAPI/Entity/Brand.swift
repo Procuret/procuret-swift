c75a387d8b3d85caf51c2ea062cc659f9361f66d
Kayla <kayla.h@procuret.com>
2021-07-23 14:06:25 -0400

    Added Brand type to Swift repository - media variable not included in struct yet.

---
+++ Sources/ProcuretAPI/Entity/Brand.swift
@@ -0,0 +1,34 @@
+//
+//  Brand.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/23/21.
+//
+
+import Foundation
+
+public struct Brand: Codable {
+    
+    let entityId: String
+    let name: String
+    let saleMessage: String
+    let created: String
+
+    
+    public enum CodingKeys: String, CodingKey {
+        case entityId = "entity_id"
+        case name = "brand_name"
+        case saleMessage = "sale_message"
+        case created
+    }
+    
+    public static func create (
+        name: String,
+        saleMessage: String,
+        entityId: String,
+        session: Session?,
+        callback: @escaping (Error?, Brand?) -> Void
+        ) {
+            fatalError("Not implemented")
+        }
+}
