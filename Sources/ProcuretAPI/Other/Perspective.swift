a95726e1bf44f7fe69bc114a4da0fa67565c1206
Kayla <kayla.h@procuret.com>
2021-07-29 15:14:10 -0400

    Added Perspective - initial struct construction only.

---
+++ Sources/ProcuretAPI/Other/Perspective.swift
@@ -0,0 +1,19 @@
+//
+//  Perspective.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/29/21.
+//
+
+import Foundation
+
+public struct Perspective: Codable {
+    
+    let indexid: Int
+    let name: String
+    
+    public enum CodingKeys: String, CodingKey {
+        case indexid
+        case name
+    }
+}
