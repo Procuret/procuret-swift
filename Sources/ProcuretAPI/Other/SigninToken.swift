0f410ae0e5c8eb686c8f6fb341c23933c3709431
Kayla <kayla.h@procuret.com>
2021-07-29 15:49:03 -0400

    created SigninToken with create function, not-implemented callback. Incomplete but compiling successfully as-is.

---
+++ Sources/ProcuretAPI/Other/SigninToken.swift
@@ -0,0 +1,30 @@
+//
+//  SigninToken.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/29/21.
+//
+
+import Foundation
+
+public struct SigninToken: Codable {
+    
+    let emailAddress: String
+    let afterSigninPath: String?
+    let perspective: Perspective
+    
+    public enum CodingKeys: String, CodingKey {
+        case emailAddress = "email_address"
+        case afterSigninPath = "after_signin_path"
+        case perspective
+    }
+    
+    public static func create (
+        emailAddress: String,
+        afterSigninPath: String?,
+        perspective: Perspective,
+        callback: @escaping (Error?) -> Void
+    ) {
+        fatalError("Not implemented")
+    }
+}
