7fb8cc1185fd25465cd073268bf77be094154ec1
Kayla <kayla.h@procuret.com>
2021-07-29 15:19:26 -0400

    added Lifecycle  - basic struct construction only.

---
+++ Sources/ProcuretAPI/Other/Lifecycle.swift
@@ -0,0 +1,19 @@
+//
+//  Lifecycle.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/29/21.
+//
+
+import Foundation
+
+public struct Lifecycle: Codable {
+    
+    let indexid: Int
+    let name: String
+    
+    public enum CodingKeys: String, CodingKey {
+        case indexid
+        case name
+    }
+}
