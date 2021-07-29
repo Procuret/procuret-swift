4458fbd82960fcdbdcb038720dbbe5e4028c911b
Kayla <kayla.h@procuret.com>
2021-07-29 17:33:45 -0400

    added Particulars - basic struct construction only. Compiling.

---
+++ Sources/ProcuretAPI/Payment/Method/Particulars.swift
@@ -0,0 +1,23 @@
+//
+//  Particulars.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/29/21.
+//
+
+import Foundation
+
+public struct Particulars: Codable {
+    
+    let endingIn: String
+    let accountName: String
+    let accountNumber: String?
+    let accountBSB: String?
+    
+    public enum CodingKeys: String, CodingKey {
+        case endingIn = "ending_in"
+        case accountName = "account_name"
+        case accountNumber = "account_number"
+        case accountBSB = "bsb_code"
+    }
+}
