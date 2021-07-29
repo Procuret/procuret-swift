8b6fb94292793bb900233cef71934eb042c3aa7c
Kayla <kayla.h@procuret.com>
2021-07-29 14:48:45 -0400

    InstalmentLinkOpen created with not-implemented create function.

---
+++ Sources/ProcuretAPI/Instalment Link/InstalmentLinkOpen.swift
@@ -0,0 +1,27 @@
+//
+//  InstalmentLinkOpen.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/29/21.
+//
+
+import Foundation
+
+public struct InstalmentLinkOpen: Codable {
+    
+    let sequence: Int
+    let created: String
+    
+    public enum CodingKeys: String, CodingKey {
+        case sequence
+        case created
+    }
+    
+    public static func create (
+        instalmentLinkId: String,
+        session: Session?,
+        callback: @escaping (Error?) -> Void
+    ) {
+        fatalError("Not implemented")
+    }
+}
