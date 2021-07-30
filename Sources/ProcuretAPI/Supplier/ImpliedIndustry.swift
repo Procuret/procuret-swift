0683f5c5a46d8d7dadcfcb631b8466887c737f8f
Kayla <kayla.h@procuret.com>
2021-07-30 12:54:04 -0400

    Implied Customer Industry added w/ path

---
+++ Sources/ProcuretAPI/Supplier/ImpliedIndustry.swift
@@ -0,0 +1,29 @@
+//
+//  ImpliedIndustry.swift
+//  
+//
+//  Created by Kayla Hoyet on 7/30/21.
+//
+
+import Foundation
+
+public struct ImpliedCustomerIndustry: Codable {
+    
+    internal static let path = "supplier/implied-customer-industry"
+    
+    let indexid: Int
+    let industryId: Int
+    
+    public enum CodingKeys: String, CodingKey {
+        case indexid
+        case industryId = "industry_id"
+    }
+    
+    public static func create(
+        supplierId: Int,
+        industryId: Int,
+        callback: @escaping (Error?, ImpliedCustomerIndustry?) -> Void
+    ) {
+            fatalError("Not implemented")
+    }
+}
