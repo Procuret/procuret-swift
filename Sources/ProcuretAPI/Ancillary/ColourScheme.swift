ea05b2a7c59fe991e837abc8d29e680f981f2d4e
Kayla <kayla.h@procuret.com>
2021-08-04 17:55:22 -0400

    added ColourScheme.

---
+++ Sources/ProcuretAPI/Ancillary/ColourScheme.swift
@@ -0,0 +1,14 @@
+//
+//  ColourScheme.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/4/21.
+//
+
+import Foundation
+
+public struct ColourScheme: Codable {
+    
+    let indexid: Int
+    let name: String
+}
