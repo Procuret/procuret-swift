bc65627ea2b76093a976dd90610c219ba68dc0ba
Kayla <kayla.h@procuret.com>
2021-08-05 17:15:32 -0400

    added ProspectivePayment - blank, w/ path.

---
+++ Sources/ProcuretAPI/Credit/ProspectivePayment.swift
@@ -0,0 +1,13 @@
+//
+//  ProspectivePayment.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/5/21.
+//
+
+import Foundation
+
+public struct ProspectivePayment: Codable {
+    
+    internal static let path = "/credit/prospective-payment"
+}
