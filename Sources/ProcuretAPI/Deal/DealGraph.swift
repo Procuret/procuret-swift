46559044ea5364c6c21fc5cda310e03589dbd684
Kayla <kayla.h@procuret.com>
2021-08-05 17:32:59 -0400

    added DealGraph - blank struct w/ path listed.

---
+++ Sources/ProcuretAPI/Deal/DealGraph.swift
@@ -0,0 +1,14 @@
+//
+//  DealGraph.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/5/21.
+//
+
+import Foundation
+
+public struct DealGraph: Codable {
+    
+    internal static let path = "/deal/graph"
+
+}
