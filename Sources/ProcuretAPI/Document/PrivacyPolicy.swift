d842a0a51ec740e8dcb24c1c213f12ebfdb535af
Kayla <kayla.h@procuret.com>
2021-08-05 17:54:03 -0400

    PrivacyPolicy added - blank struct file w/ path.

---
+++ Sources/ProcuretAPI/Document/PrivacyPolicy.swift
@@ -0,0 +1,13 @@
+//
+//  PrivacyPolicy.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/5/21.
+//
+
+import Foundation
+
+public struct PrivacyPolicy: Codable {
+    
+    internal static let path = "/document/privacy-policy"
+}
