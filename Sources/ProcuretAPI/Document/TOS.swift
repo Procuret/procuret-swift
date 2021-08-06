2ff735573524a24f98fe9a17fbb0755c3ab5d09e
Kayla <kayla.h@procuret.com>
2021-08-06 17:58:28 -0400

    added Document/TOS - Blank struct file - path added.

---
+++ Sources/ProcuretAPI/Document/TOS.swift
@@ -0,0 +1,14 @@
+//
+//  TOS.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/6/21.
+//
+
+import Foundation
+
+public struct TOS: Codable {
+    
+    internal static let path = "/document/terms-of-service"
+    
+}
