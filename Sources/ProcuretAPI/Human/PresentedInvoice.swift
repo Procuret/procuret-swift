f6b25558306fbe9ede6a69d16d8f583c61f53210
Kayla <kayla.h@procuret.com>
2021-08-09 18:28:18 -0400

    path added to PresentedInvoice. api request (not-implemented) added to create func in PresentedInvoice.

--- Sources/ProcuretAPI/Human/PresentedInvoice.swift
+++ Sources/ProcuretAPI/Human/PresentedInvoice.swift
@@ -9,13 +9,15 @@ import Foundation
 
 public struct PresentedInvoice: Codable {
     
+    internal static let path = "/human/presented-invoice"
+    
     let publicId: String
     
     public enum CodingKeys: String, CodingKey {
         case publicId = "public_id"
     }
     
-    public static func create (
+    public static func create(
         amount: Decimal,
         supplierId: Int,
         invoiceIdentifier: String,
@@ -23,6 +25,29 @@ public struct PresentedInvoice: Codable {
         session: Session?,
         callback: @escaping (Error?, Human?) -> Void
     ) {
-        fatalError("Not implemented")
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(amount: amount,
+                supplierId: supplierId, invoiceIdentifier:
+                invoiceIdentifier, preferredPeriods: preferredPeriods),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    private struct CreatePayload: Codable {
+        let amount: Decimal
+        let supplierId: Int
+        let invoiceIdentifier: String
+        let preferredPeriods: Int
+        
+        private enum CodingKeys: String, CodingKey {
+            case amount
+            case supplierId = "supplier_id"
+            case invoiceIdentifier = "identifier"
+            case preferredPeriods = "preferred_periods"
+        }
     }
 }
