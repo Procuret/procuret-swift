59e9d3b931b8b949ba267c865418af4455f1cbc3
Kayla <kayla.h@procuret.com>
2021-08-11 17:19:58 -0400

    added ShopifyAuthorisationRequest - w/ path, create func, non-implemented api call - compiling.

---
+++ Sources/ProcuretAPI/Shopify/AuthRequest.swift
@@ -0,0 +1,52 @@
+//
+//  AuthRequest.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct ShopifyAuthorisationRequest: Codable {
+    
+    internal static let path = "/shopify/authorisation-request"
+    
+    let loginUrl: String
+    let publicId: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case loginUrl = "login_url"
+        case publicId = "public_id"
+    }
+    
+    public static func create(
+        supplierId: String,
+        shopName: String,
+        beneficiaryAgentId: String,
+        session: Session?,
+        callback: @escaping (Error?, Self?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(supplierId: supplierId,
+                shopName: shopName, beneficiaryAgentId: beneficiaryAgentId),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let supplierId: String
+        let shopName: String
+        let beneficiaryAgentId: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case supplierId = "supplier_id"
+            case shopName = "shop_name"
+            case beneficiaryAgentId = "beneficiary_agent_id"
+        }
+    }
+}
