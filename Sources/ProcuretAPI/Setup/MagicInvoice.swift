2194f99a01a20229969475313cade1930d17c73c
Kayla <kayla.h@procuret.com>
2021-08-11 15:59:48 -0400

    added MagicInvoice - empty struct w/ path - compiling.

---
+++ Sources/ProcuretAPI/Setup/MagicInvoice.swift
@@ -0,0 +1,13 @@
+//
+//  MagicInvoice.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct MagicInvoice: Codable {
+    
+    internal static let path = "/magic-invoice"
+}
