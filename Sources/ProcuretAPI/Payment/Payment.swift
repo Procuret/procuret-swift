b042af7f5fc3d065209e321a4f72cb1eef4c723a
Kayla <kayla.h@procuret.com>
2021-08-11 12:46:47 -0400

    added Payment - w/path - basic struct file, no meat yet. Compiling.

---
+++ Sources/ProcuretAPI/Payment/Payment.swift
@@ -0,0 +1,13 @@
+//
+//  Payment.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct Payment: Codable {
+    
+    internal static let path = "/payment"
+}
