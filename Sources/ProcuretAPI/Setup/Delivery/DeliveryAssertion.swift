d02732495acd6bf0dcc612f6f707c22c0490bd9c
Kayla <kayla.h@procuret.com>
2021-08-11 15:49:11 -0400

    added DeliveryAssertion file with path - blank struct, but compiling.

---
+++ Sources/ProcuretAPI/Setup/Delivery/DeliveryAssertion.swift
@@ -0,0 +1,13 @@
+//
+//  DeliveryAssertion.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct DeliveryAssertion: Codable {
+    
+    internal static let path = "/setup/delivery/assertion"
+}
