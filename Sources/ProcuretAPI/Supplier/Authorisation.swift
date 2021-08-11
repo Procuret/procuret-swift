50a2e26ba08888db8fc3f92b43f95f46ecabd4d6
Kayla <kayla.h@procuret.com>
2021-08-11 17:41:33 -0400

    added SupplierAuthorisation - w/ path, create func, non-implemented api call (compiling)

---
+++ Sources/ProcuretAPI/Supplier/Authorisation.swift
@@ -0,0 +1,48 @@
+//
+//  Authorisation.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct SupplierAuthorisation: Codable {
+    
+    internal static let path = "/supplier/authorisation"
+    
+    let supplierId: String
+    let authorise: Bool
+    
+    private enum CodingKeys: String, CodingKey {
+        case supplierId = "supplier_id"
+        case authorise
+    }
+    
+    public static func create(
+        supplierId: String,
+        authorise: Bool,
+        session: Session?,
+        callback:  @escaping (Error?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(supplierId: supplierId, authorise: authorise),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let supplierId: String
+        let authorise: Bool
+        
+        private enum CodingKeys: String, CodingKey {
+            case supplierId = "supplier_id"
+            case authorise
+        }
+    }
+}
