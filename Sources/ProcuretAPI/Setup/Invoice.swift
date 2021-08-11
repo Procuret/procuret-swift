4e17751ba81a5c9c30607f3ad0142fc8bd7250a7
Kayla <kayla.h@procuret.com>
2021-08-11 15:56:43 -0400

    added Invoice - blank struct for now, compiling.

---
+++ Sources/ProcuretAPI/Setup/Invoice.swift
@@ -0,0 +1,14 @@
+//
+//  Invoice.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct Invoice: Codable {
+    
+    internal static let path = "/invoice"
+    
+}
