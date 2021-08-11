427372e02701fc8c0d20eccdaa7e1763f0691348
Kayla <kayla.h@procuret.com>
2021-08-11 15:52:03 -0400

    added DeliveryResponse - blank struct w/ path. Compiling.

---
+++ Sources/ProcuretAPI/Setup/Delivery/DeliveryResponse.swift
@@ -0,0 +1,14 @@
+//
+//  DeliveryResponse.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct DeliveryResponse: Codable {
+    
+    internal static let path = "/setup/delivery/response"
+    
+}
