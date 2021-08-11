a91f91be68789e18437d4ec7a7a1ec034e8a6535
Kayla <kayla.h@procuret.com>
2021-08-11 15:38:07 -0400

    added Margin - basic struct w/ path.

---
+++ Sources/ProcuretAPI/Product/Margin.swift
@@ -0,0 +1,13 @@
+//
+//  Margin.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct Margin: Codable {
+    
+    internal static let path = "/margin"
+}
