ea00adb9b5f5678e704f6c082e752da228fba91d
Kayla <kayla.h@procuret.com>
2021-08-11 16:18:33 -0400

    added basic struct SetupInvoice w/ path. Compiling.

---
+++ Sources/ProcuretAPI/Setup/SetupInvoice.swift
@@ -0,0 +1,21 @@
+//
+//  SetupInvoice.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/11/21.
+//
+
+import Foundation
+
+public struct SetupInvoice: Codable {
+    
+    internal static let path = "/setup/invoice"
+    
+    let setupId: String
+    let invoiceId: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case setupId = "setup_id"
+        case invoiceId = "invoice_id"
+    }
+}
