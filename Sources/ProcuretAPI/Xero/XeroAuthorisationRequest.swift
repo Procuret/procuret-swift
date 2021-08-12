caf7dca02e5634cbcfdaf36c7ed47726df6e481b
Kayla <kayla.h@procuret.com>
2021-08-12 17:02:37 -0400

    added XeroAuthorisationRequest - blank struct w/ path. Compiling.

---
+++ Sources/ProcuretAPI/Xero/XeroAuthorisationRequest.swift
@@ -0,0 +1,13 @@
+//
+//  XeroAuthorisationRequest.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/12/21.
+//
+
+import Foundation
+
+public struct XeroAuthorisationRequest: Codable {
+    
+    internal static let path = "/xero/authorisation-request"
+}
