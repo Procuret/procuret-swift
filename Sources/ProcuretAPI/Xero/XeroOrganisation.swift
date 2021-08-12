18fd010aef79fbe2a7a181fc975b827108f8df9d
Kayla <kayla.h@procuret.com>
2021-08-12 17:12:15 -0400

    added XeroOrganisation - w path, create func, non-implemented API call. Incomplete but compiling.

---
+++ Sources/ProcuretAPI/Xero/XeroOrganisation.swift
@@ -0,0 +1,46 @@
+//
+//  XeroOrganisation.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/12/21.
+//
+
+import Foundation
+
+public struct XeroOrganisation: Codable {
+    
+    internal static let path = "/xero/organisation"
+    
+    let entityId: String
+    let shortCode: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case entityId = "entity_id"
+        case shortCode = "shortcode"
+    }
+    
+    public static func create(
+        entityId: String,
+        session: Session?,
+        callback: @escaping (Error?, XeroOrganisation?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(entityId: entityId),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+                fatalError("Not implemented")
+        }
+    }
+                
+    private struct CreatePayload: Codable {
+        let entityId: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case entityId = "entity_id"
+
+        }
+    }
+}
