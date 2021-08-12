0d56f60fc99c5fc48f19b45b6132b695ff34234f
Kayla <kayla.h@procuret.com>
2021-08-12 15:44:08 -0400

    added Supplier/Product - blank struct file w/ path. Compiling.

---
+++ Sources/ProcuretAPI/Supplier/Product.swift
@@ -0,0 +1,13 @@
+//
+//  Procuct.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/12/21.
+//
+
+import Foundation
+
+public struct Product: Codable {
+    
+    internal static let path = "/supplier/product"
+}
