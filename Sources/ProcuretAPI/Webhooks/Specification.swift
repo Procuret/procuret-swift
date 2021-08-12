81fe3ac552b67b483e6999eb33a0ecd4bf1afab2
Kayla <kayla.h@procuret.com>
2021-08-12 16:50:59 -0400

    added Webhooks/Specification - blank struct file w/ path. Compiling.

---
+++ Sources/ProcuretAPI/Webhooks/Specification.swift
@@ -0,0 +1,13 @@
+//
+//  Specification.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/12/21.
+//
+
+import Foundation
+
+public struct Specification: Codable {
+    
+    internal static let path = "/webhook/specification"
+}
