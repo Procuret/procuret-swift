6db5a44e2bbd1bfed950b61978c65ca06161773d
Kayla <kayla.h@procuret.com>
2021-08-12 16:14:55 -0400

    added Member - blank struct w/ path - compiling.

---
+++ Sources/ProcuretAPI/Team/Member.swift
@@ -0,0 +1,13 @@
+//
+//  Member.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/12/21.
+//
+
+import Foundation
+
+public struct Member: Codable {
+    
+    internal static let path = "/entity/team/member"
+}
