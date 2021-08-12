222217a71afe88b60abf6c63c1b7aebbacd9a84a
Kayla <kayla.h@procuret.com>
2021-08-12 15:51:02 -0400

    added SettlementAccount - w/ path, create func, non-implemented API call - compiling.

---
+++ Sources/ProcuretAPI/Supplier/SettlementAccount.swift
@@ -0,0 +1,53 @@
+//
+//  SettlementAccount.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/12/21.
+//
+
+import Foundation
+
+public struct SettlementAccount: Codable {
+    
+    internal static let path = Supplier.path + "/settlement-account"
+    
+    let created: ProcuretTime
+    let supplier: EntityHeadline
+    let creator: HumanHeadline
+    let account: DirectDebit
+    
+    private enum CodingKeys: String, CodingKey {
+        case created
+        case supplier
+        case creator
+        case account
+    }
+    
+    public static func create(
+        supplierId: String,
+        accountId: String,
+        session: Session?,
+        callback: @escaping (Self?, Error?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(supplierId: supplierId,
+                accountId: accountId),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+                fatalError("Not implemented")
+        }
+    }
+            
+    private struct CreatePayload: Codable {
+        let supplierId: String
+        let accountId: String
+                
+        private enum CodingKeys: String, CodingKey {
+            case supplierId = "supplier_id"
+            case accountId = "account_id"
+        }
+    }
+}
