d7a71dc7e02ce991a8ab10fc95240b9211be99e6
Kayla <kayla.h@procuret.com>
2021-08-12 16:10:27 -0400

    added SupplierTOS - blank struct w/ path.

---
+++ Sources/ProcuretAPI/Supplier/SupplierTOS.swift
@@ -0,0 +1,14 @@
+//
+//  SupplierTOS.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/12/21.
+//
+
+import Foundation
+
+public struct SupplierTOS: Codable {
+    
+    internal static let path = "/supplier/tos"
+    
+}
