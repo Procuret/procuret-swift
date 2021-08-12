b3cf657b8d5e4900041bff7e0fd2cb90d71b3ed1
Kayla <kayla.h@procuret.com>
2021-08-12 17:06:40 -0400

    added XeroEntityMap - blank struct file w/ path. Compiling.

---
+++ Sources/ProcuretAPI/Xero/XeroEntityMap.swift
@@ -0,0 +1,13 @@
+//
+//  XeroEntityMap.swift
+//  
+//
+//  Created by Kayla Hoyet on 8/12/21.
+//
+
+import Foundation
+
+public struct XeroEntityMap: Codable {
+    
+    internal static let path = "/xero/entity-map"
+}
