73457a9a472bf36490e54589a6bbb057757bc1fd
Kayla <kayla.h@procuret.com>
2021-08-18 15:24:24 -0400

    populated DealGraph struct. Added DealGraphAttribute and DealGraphGroupBy to file. Blank but compiling.

--- Sources/ProcuretAPI/Deal/DealGraph.swift
+++ Sources/ProcuretAPI/Deal/DealGraph.swift
@@ -10,5 +10,29 @@ import Foundation
 public struct DealGraph: Codable {
     
     internal static let path = "/deal/graph"
+    
+    let start: ProcuretTime
+    let end: ProcuretTime
+    let attribute: DealGraphAttribute
+    let groupBy: DealGraphGroupBy
+    let yValues: Array<Float>
+    let xLabels: Array<String>
+    
+    private enum CodingKeys: String, CodingKey {
+        case start
+        case end
+        case attribute
+        case groupBy
+        case yValues = "y_values"
+        case xLabels = "x_labels"
+    }
 
+}
+
+public struct DealGraphAttribute: Codable {
+    
+}
+
+public struct DealGraphGroupBy: Codable {
+    
 }
