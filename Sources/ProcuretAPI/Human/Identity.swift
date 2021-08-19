d2330b433f3fe37bd050efc05e18139feef6dbf1
Kayla <kayla.h@procuret.com>
2021-08-19 16:38:18 -0400

    formatting updated on Identity.Swift.

--- Sources/ProcuretAPI/Human/Identity.swift
+++ Sources/ProcuretAPI/Human/Identity.swift
@@ -8,12 +8,11 @@
 import Foundation
 
 public struct HumanIdentity: Codable {
-  let dateOfBirth: String?
-  let address: Address?
+    let dateOfBirth: String?
+    let address: Address?
     
-  private enum CodingKeys: String, CodingKey {
-    case dateOfBirth = "date_of_birth"
-    case address
-  }
-    
+    private enum CodingKeys: String, CodingKey {
+        case dateOfBirth = "date_of_birth"
+        case address
+    }
 }
