034cd13a51a24b5ea8c719338d9a26ff048648cd
Kayla <kayla.h@procuret.com>
2021-08-19 16:13:32 -0400

    EntityPermission updated - added retrieve function w/ non-implemented API call. Compiling.

--- Sources/ProcuretAPI/Entity/EntityPermission.swift
+++ Sources/ProcuretAPI/Entity/EntityPermission.swift
@@ -10,6 +10,7 @@ import Foundation
 public struct EntityPermission: Codable {
     
     internal static let path = "/entity/permission"
+    internal static let listPath = EntityPermission.path + "/list"
     
     let entity: EntityHeadline
     let human: HumanHeadline
@@ -44,6 +45,24 @@ public struct EntityPermission: Codable {
         }
     }
     
+    public static func retrieve(
+        entityId: String,
+        humanId: String,
+        session: Session?,
+        callback: @escaping (Error?, Self?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: RetrieveParameters(entityId: entityId,
+                humanId: humanId),
+            session: session,
+            query: nil,
+            method: .GET
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
     private struct CreatePayload: Codable {
         let humanId: String
         let entityId: String
@@ -53,6 +72,16 @@ public struct EntityPermission: Codable {
             case humanId = "human_id"
             case entityId = "entity_id"
             case privileges
+        }
+    }
+    
+    private struct RetrieveParameters: Codable {
+        let entityId: String
+        let humanId: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case entityId = "entity_id"
+            case humanId = "human_id"
         }
     }
 }
