dc9f8ac015d6e86e26b8fbe954ead67e18be8795
Kayla <kayla.h@procuret.com>
2021-08-19 11:55:30 -0400

    name const and create function (non-implemented) added to TOS document. Incomplete but compiling.

--- Sources/ProcuretAPI/Document/TOS.swift
+++ Sources/ProcuretAPI/Document/TOS.swift
@@ -10,5 +10,12 @@ import Foundation
 public struct TOS: Codable {
     
     internal static let path = "/document/terms-of-service"
+    internal static let name = "Terms of Service"
     
+    public static func create(
+        markdownBody: String,
+        callback: @escaping (Error, DocumentRevision) -> Void
+    ) {
+        fatalError("Not implemented")
+    }
 }
