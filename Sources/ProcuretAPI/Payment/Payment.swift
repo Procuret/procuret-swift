87b6ae68635d046bdddbe2a0e1eca67c2b198723
Kayla <kayla.h@procuret.com>
2021-08-20 13:50:56 -0400

    added let 'insufficient_funds' to Payment.Swift. file incomplete, but compiling.

--- Sources/ProcuretAPI/Payment/Payment.swift
+++ Sources/ProcuretAPI/Payment/Payment.swift
@@ -11,4 +11,11 @@ public struct Payment: Codable {
     
     internal static let path = "/payment"
     
+    internal static let insufficient_funds: String =
+    """
+    Your financial institution reports that
+    your card has insufficient funds available. You could try adding
+    funds, or try using a different card.
+    """
+
 }
