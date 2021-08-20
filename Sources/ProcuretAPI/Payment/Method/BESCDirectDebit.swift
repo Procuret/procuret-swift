a30adc8bb3a4ed6ef125ee7920ed54264b1db566
Kayla <kayla.h@procuret.com>
2021-08-20 17:07:54 -0400

    added non-implemented api call to BECSDirectDebit. Compiling, but incomplete.

--- Sources/ProcuretAPI/Payment/Method/BESCDirectDebit.swift
+++ Sources/ProcuretAPI/Payment/Method/BESCDirectDebit.swift
@@ -30,6 +30,37 @@ public struct BECSDirectDebit: Codable {
         entityId: Int?,
         callback: @escaping (Error?, BECSDirectDebit?) -> Void
     ) {
-        fatalError("Not implemented")
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(bsbCode: bsbCode,
+                accountNumber: accountNumber,
+                accountName: accountName,
+                authorityAgentId: authorityAgentId,
+                timeMandateAccepted: timeMandateAccepted,
+                entityId: entityId),
+            session: nil,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let bsbCode: String
+        let accountNumber: String
+        let accountName: String
+        let authorityAgentId: Int
+        let timeMandateAccepted: Int
+        let entityId: Int?
+        
+        private enum CodingKeys: String, CodingKey {
+            case bsbCode = "bsb"
+            case accountNumber = "account_number"
+            case accountName = "account_name"
+            case authorityAgentId = "authority_agent_id"
+            case timeMandateAccepted = "time_mandate_accepted"
+            case entityId = "entity_id"
+        }
     }
 }
