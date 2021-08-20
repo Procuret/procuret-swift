d3594730e12d6f36b3b6153424a6212d14744f2a
Kayla <kayla.h@procuret.com>
2021-08-20 17:20:06 -0400

    added non-implemented api call to create function on DesignatedPaymentMethod. Compiling.

--- Sources/ProcuretAPI/Payment/Method/Designated.swift
+++ Sources/ProcuretAPI/Payment/Method/Designated.swift
@@ -9,6 +9,8 @@ import Foundation
 
 public struct DesignatedPaymentMethod: Codable {
     
+    internal static let path = PaymentSeries.path + "/designated-method"
+    
     let seriesId: String
     let methodId: String
     
@@ -23,6 +25,24 @@ public struct DesignatedPaymentMethod: Codable {
         session: Session?,
         callback: @escaping (Error?) -> Void
     ) {
-        fatalError("Not implemented")
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(seriesId: seriesId, methodId: methodId),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+        
+    private struct CreatePayload: Codable {
+        let seriesId: String
+        let methodId: String
+            
+        private enum CodingKeys: String, CodingKey {
+            case seriesId = "series_id"
+            case methodId = "method_id"
+        }
     }
 }
