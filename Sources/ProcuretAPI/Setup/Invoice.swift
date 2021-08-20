b2d7083bebbab7e479551fbec1ed3932295c83b9
Kayla <kayla.h@procuret.com>
2021-08-20 18:53:30 -0400

    added create function with non-implemented api call to Invoice.Swift.

--- Sources/ProcuretAPI/Setup/Invoice.swift
+++ Sources/ProcuretAPI/Setup/Invoice.swift
@@ -11,4 +11,54 @@ public struct Invoice: Codable {
     
     internal static let path = "/invoice"
     
+    internal static func create(
+        supplierId: Int,
+        invoiceFilename: String,
+        invoiceIdentifier: String,
+        contentDescription: String,
+        invoiceeBusinessId: Int?,
+        deliveredDate: Date?,
+        invoiceBase64: String,
+        magnitudeValue: Decimal,
+        callback: @escaping (Error?, Invoice?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(supplierId: supplierId,
+                invoiceFilename: invoiceFilename,
+                invoiceIdentifier: invoiceIdentifier,
+                contentDescription: contentDescription,
+                invoiceeBusinessId: invoiceeBusinessId,
+                deliveredDate: deliveredDate,
+                invoiceBase64: invoiceBase64,
+                magnitudeValue: magnitudeValue),
+            session: nil,
+            query: nil,
+            method: .GET
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let supplierId: Int
+        let invoiceFilename: String
+        let invoiceIdentifier: String
+        let contentDescription: String
+        let invoiceeBusinessId: Int?
+        let deliveredDate: Date?
+        let invoiceBase64: String
+        let magnitudeValue: Decimal
+        
+        private enum CodingKeys: String, CodingKey {
+            case supplierId = "supplier_id"
+            case invoiceFilename = "invoice_filename"
+            case invoiceIdentifier = "invoice_identifier"
+            case contentDescription = "content_description"
+            case invoiceeBusinessId = "invoicee_business_id"
+            case deliveredDate = "delivered_date"
+            case invoiceBase64 = "invoice_base64"
+            case magnitudeValue = "magnitude_value"
+        }
+    }
 }
