50e40dff0af1b7474d8d7b8734715f6a3dda9ca8
Kayla <kayla.h@procuret.com>
2021-08-20 18:12:51 -0400

    let const listPath added, retrieve function w/ non-implemented api call added to PaymentMethod. Compiling.

--- Sources/ProcuretAPI/Payment/Method/Method.swift
+++ Sources/ProcuretAPI/Payment/Method/Method.swift
@@ -10,5 +10,29 @@ import Foundation
 public struct PaymentMethod: Codable {
     
     internal static let path = "/payment/method"
+    internal static let listPath = PaymentMethod.path + "/list"
     
+    internal static func retrieve(
+        publicId: String,
+        session: Session?,
+        callback: @escaping (Error?, PaymentMethod?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: RetrieveParameters(publicId: publicId),
+            session: session,
+            query: nil,
+            method: .GET
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct RetrieveParameters: Codable {
+        let publicId: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case publicId = "public_id"
+        }
+    }
 }
