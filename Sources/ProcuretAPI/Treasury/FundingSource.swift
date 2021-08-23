55611d6c4164eb01134a41a4511ee2acbb85a511
Kayla <kayla.h@procuret.com>
2021-08-23 14:41:27 -0400

    retrieve function with non-implemented api call added to FundingSource. Compiling.

--- Sources/ProcuretAPI/Treasury/FundingSource.swift
+++ Sources/ProcuretAPI/Treasury/FundingSource.swift
@@ -10,6 +10,7 @@ import Foundation
 public struct FundingSource: Codable {
     
     internal static let path = "/treasury/funding-source"
+    internal static let listPath = FundingSource.path + "/list"
     
     let indexid: Int
     let name: String
@@ -36,12 +37,36 @@ public struct FundingSource: Codable {
                 fatalError("Not implemented")
         }
     }
+    
+    public static func retrieve(
+        indexid: Int,
+        session: Session?,
+        callback: @escaping (Error?, Self?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: RetrieveParameters(indexid: indexid),
+            session: session,
+            query: nil,
+            method: .GET
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
                 
     private struct CreatePayload: Codable {
         let name: String
                     
         private enum CodingKeys: String, CodingKey {
             case name
+        }
+    }
+    
+    private struct RetrieveParameters: Codable {
+        let indexid: Int
+        
+        private enum CodingKeys: String, CodingKey {
+            case indexid
         }
     }
 }
