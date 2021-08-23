3054d43d7b3bc9e35e755c7e144f0bfb1754edc2
Kayla <kayla.h@procuret.com>
2021-08-23 15:07:29 -0400

    retrieveList function with non-implemented api call added to XeroBrandingTheme. Compiling.

--- Sources/ProcuretAPI/Xero/XeroBrandingTheme.swift
+++ Sources/ProcuretAPI/Xero/XeroBrandingTheme.swift
@@ -18,4 +18,28 @@ public struct XeroBrandingTheme: Codable {
         case themeId = "theme_id"
         case name
     }
+    
+    public static func retrieveList(
+        entityId: String,
+        session: Session?,
+        callback: @escaping (Error?, Array<XeroBrandingTheme>?) -> Void
+    ) {
+        Request.make(
+            path: self.path + "/list",
+            payload: RetrieveListParameters(entityId: entityId),
+            session: session,
+            query: nil,
+            method: .GET
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct RetrieveListParameters: Codable {
+        let entityId: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case entityId = "entity_id"
+        }
+    }
 }
