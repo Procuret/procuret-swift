7dfba33e7dd0ca2f530b392d1ef23706c92a4408
Kayla <kayla.h@procuret.com>
2021-08-23 15:20:09 -0400

    retrieve function with non-implemented api call added to XeroOrganisation. Compiling.

--- Sources/ProcuretAPI/Xero/XeroOrganisation.swift
+++ Sources/ProcuretAPI/Xero/XeroOrganisation.swift
@@ -34,6 +34,22 @@ public struct XeroOrganisation: Codable {
                 fatalError("Not implemented")
         }
     }
+    
+    public static func retrieve(
+        entityId: String,
+        session: Session?,
+        callback: @escaping (Error?, XeroOrganisation?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: RetrieveParameters(entityId: entityId),
+            session: session,
+            query: nil,
+            method: .GET
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
                 
     private struct CreatePayload: Codable {
         let entityId: String
@@ -41,6 +57,14 @@ public struct XeroOrganisation: Codable {
         private enum CodingKeys: String, CodingKey {
             case entityId = "entity_id"
 
+        }
+    }
+    
+    private struct RetrieveParameters: Codable {
+        let entityId: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case entityId = "entity_id"
         }
     }
 }
