309dca208370cdb33e8f00c47a7414c4eb4aa1f1
Kayla <kayla.h@procuret.com>
2021-08-23 15:13:07 -0400

    create function with non-implemented api call added to XeroEntityMap. Compiling.

--- Sources/ProcuretAPI/Xero/XeroEntityMap.swift
+++ Sources/ProcuretAPI/Xero/XeroEntityMap.swift
@@ -10,4 +10,31 @@ import Foundation
 public struct XeroEntityMap: Codable {
     
     internal static let path = "/xero/entity-map"
+    
+    public static func create(
+        entityId: String,
+        tenantId: String,
+        session: Session?,
+        callback: @escaping (Error?, XeroEntityMap?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(entityId: entityId, tenantId: tenantId),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let entityId: String
+        let tenantId: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case entityId = "entity_id"
+            case tenantId = "tenant_id"
+        }
+    }
 }
