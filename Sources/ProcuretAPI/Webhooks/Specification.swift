8836ad2be112fe44a11f9d5a92dda374a2fbd9c5
Kayla <kayla.h@procuret.com>
2021-08-23 14:57:09 -0400

    create function added to Specification - callback function missing 'object' - compiling but incomplete.

--- Sources/ProcuretAPI/Webhooks/Specification.swift
+++ Sources/ProcuretAPI/Webhooks/Specification.swift
@@ -10,4 +10,33 @@ import Foundation
 public struct Specification: Codable {
     
     internal static let path = "/webhook/specification"
+    internal static let testPath = "/webhook/specification/test"
+    
+    public static func create(
+        entityId: Int,
+        destination: String,
+        secret: String,
+        formatId: Int,
+        eventIds: Array<Int>,
+        callback: @escaping (Error?) -> Void
+    ) {
+        fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let entityId: Int
+        let destination: String
+        let secret: String
+        let formatId: Int
+        let eventIds: Array<Int>
+        
+        private enum CodingKeys: String, CodingKey {
+            case entityId = "entity_id"
+            case destination
+            case secret
+            case formatId = "format_id"
+            case eventIds = "event_ids"
+    }
 }
+
