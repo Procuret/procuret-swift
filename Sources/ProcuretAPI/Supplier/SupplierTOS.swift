6d1f53a0cc52ebd2ead3fdfa900b38e860513685
Kayla <kayla.h@procuret.com>
2021-08-23 14:26:55 -0400

    create function with non-implemented api call added to SupplierTOS. Compiling.

--- Sources/ProcuretAPI/Supplier/SupplierTOS.swift
+++ Sources/ProcuretAPI/Supplier/SupplierTOS.swift
@@ -11,4 +11,30 @@ public struct SupplierTOS: Codable {
     
     internal static let path = "/supplier/tos"
     
+    public static func create(
+        supplierId: Int,
+        markdown: String,
+        callback: @escaping (Error?, SupplierTOS?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(supplierId: supplierId,
+                markdown: markdown),
+            session: nil,
+            query: nil,
+            method: .POST
+        ) { error, data in
+                fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let supplierId: Int
+        let markdown: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case supplierId = "supplier_id"
+            case markdown
+        }
+    }
 }
