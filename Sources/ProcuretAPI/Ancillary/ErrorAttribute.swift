2a35d3c1182d0c0c2988ce76c68ad25d25704261
Kayla <kayla.h@procuret.com>
2021-10-26 15:49:48 -0400

    ErrorInstanceLog and ErrorAttribute added - blank struct files. Compiling.

---
+++ Sources/ProcuretAPI/Ancillary/ErrorAttribute.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorAttribute.swift
+//  
+//
+//  Created by Kayla Hoyet on 10/26/21.
+//
+
+import Foundation
+
+public struct ErrorAttribute: Codable {
+    
+}
---
+++ Sources/ProcuretAPI/Ancillary/ErrorInstanceLog.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorInstanceLog.swift
+//  
+//
+//  Created by Kayla Hoyet on 10/26/21.
+//
+
+import Foundation
+
+public struct ErrorInstanceLog: Codable {
+    
+}
