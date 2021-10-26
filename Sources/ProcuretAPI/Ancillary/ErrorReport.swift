c0bc05bf7a4bf7da0b2b02a38307c7029b39e803
Kayla <kayla.h@procuret.com>
2021-10-26 16:03:10 -0400

    ErrorReport struct w/ non-implemented create function. Compiling.

--- Sources/ProcuretAPI/Ancillary/ErrorReport.swift
+++ Sources/ProcuretAPI/Ancillary/ErrorReport.swift
@@ -9,4 +9,44 @@ import Foundation
 
 public struct ErrorReport: Codable {
     
+    internal static let path = "/error-report"
+    internal static let listPath = ErrorReport.path + "/list"
+    
+    let indexid: Int
+    let created: String //DateTime
+    let applicationName: String
+    let attributes: Array<ErrorInstanceLog>
+    let disposition: Disposition
+    
+    private enum CodingKeys: String, CodingKey {
+        case indexid
+        case created
+        case applicationName = "application_name"
+        case attributes
+        case disposition
+    }
+    
+    public static func create(
+        attributes: Array<ErrorAttribute>
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(
+                attributes: attributes
+            ),
+            session: nil,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let attributes: Array<ErrorAttribute>
+        
+        private enum CodingKeys: String, CodingKey {
+            case attributes
+        }
+    }
 }
