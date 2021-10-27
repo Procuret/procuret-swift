b182475c081dc174ebf5279be5d1ead1a0f20621
Kayla <kayla.h@procuret.com>
2021-10-27 18:34:42 -0400

    update to ProspectivePayment - added  non-implemented retrieve function. Compiling.

--- Sources/ProcuretAPI/Credit/ProspectivePayment.swift
+++ Sources/ProcuretAPI/Credit/ProspectivePayment.swift
@@ -10,4 +10,40 @@ import Foundation
 public struct ProspectivePayment: Codable {
     
     internal static let path = "/credit/prospective-payment"
+    
+    public static func retrieve(
+        principle: Float,
+        cycle: Cycle,
+        supplierId: Int,
+        periods: Int,
+        callback: @escaping (Error?, ProspectivePayment?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: retrieveParameters(
+                principle: principle,
+                cycle: cycle,
+                supplierId: supplierId,
+                periods: periods),
+            session: nil,
+            query: nil,
+            method: .GET
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct retrieveParameters: Codable {
+        let principle: Float
+        let cycle: Cycle
+        let supplierId: Int
+        let periods: Int
+        
+        private enum CodingKeys: String, CodingKey {
+            case principle
+            case cycle
+            case supplierId = "supplier_id"
+            case periods
+        }
+    }
 }
