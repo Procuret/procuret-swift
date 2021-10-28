c34156665a789275fe1a83c3b0d9512187ec637c
Kayla <kayla.h@procuret.com>
2021-10-28 15:46:42 -0400

    added Business.js with create function. Need to confirm types. Compiling.

---
+++ Sources/ProcuretAPI/Business/Business.swift
@@ -0,0 +1,51 @@
+//
+//  Business.swift
+//  
+//
+//  Created by Kayla Hoyet on 10/28/21.
+//
+
+import Foundation
+
+public struct Business: Codable {
+    
+    internal static let path = "/business"
+    
+    let entity: Entity //need to confirm type
+    
+    private enum CodingKeys: String, CodingKey {
+        case entity
+    }
+    
+    public static func create(
+        abn: String,
+        address: Address, //may change to AddressCreationStruct
+        session: Session?,
+        callback: @escaping (Error?, Business?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(
+                abn: abn,
+                address: address
+            ),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            Request.decodeResponse(error, data, self, callback)
+            return
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let abn: String
+        let address: Address
+        
+        private enum CodingKeys: String, CodingKey {
+            case abn
+            case address
+        }
+    }
+
+}
