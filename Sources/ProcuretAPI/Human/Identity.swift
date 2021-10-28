af5d4a77c60953779984d92286dc698281e8466e
Kayla <kayla.h@procuret.com>
2021-10-28 16:00:27 -0400

    updated HumanIdentity with create func. Need to confirm types. Compiling.

--- Sources/ProcuretAPI/Human/Identity.swift
+++ Sources/ProcuretAPI/Human/Identity.swift
@@ -1,5 +1,5 @@
 //
-//  Identity.swift
+//  HumanIdentity.swift
 //  
 //
 //  Created by Kayla Hoyet on 7/14/21.
@@ -8,11 +8,47 @@
 import Foundation
 
 public struct HumanIdentity: Codable {
+    
+    internal static let path = "/human/identity"
+    
     let dateOfBirth: String?
     let address: Address?
     
     private enum CodingKeys: String, CodingKey {
         case dateOfBirth = "date_of_birth"
         case address
+    }
+    
+    public static func create(
+        humanId: String,
+        dateOfBirth: Date,
+        address: Address,
+        session: Session,
+        callback: @escaping (Error?, Self?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(
+                humanId: humanId,
+                dateOfBirth: dateOfBirth,
+                address: address),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            fatalError("Not implemented")
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let humanId: String
+        let dateOfBirth: Date
+        let address: Address
+        
+        private enum CodingKeys: String, CodingKey {
+            case humanId = "human_id"
+            case dateOfBirth = "date_of_birth"
+            case address
+        }
     }
 }
