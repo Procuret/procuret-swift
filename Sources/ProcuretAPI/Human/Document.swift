15e25300af5bb2a36b3dd102cb01f29e20e3a6ef
Kayla <kayla.h@procuret.com>
2021-10-28 17:15:14 -0400

    added create function to HumanIdentityDocument w/ IdentityDocumentType. Compiling.

--- Sources/ProcuretAPI/Human/Document.swift
+++ Sources/ProcuretAPI/Human/Document.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 public struct HumanIdentityDocument: Codable {
+    
+    internal static let path = "/human/identity-document"
+    
     let documentTypeId: Int
     let documentName: String
     let documentIdentifier: String
@@ -20,5 +23,40 @@ public struct HumanIdentityDocument: Codable {
         case documentIdentifier = "document_identifier"
         case stateID = "state_id"
         case stateName = "state_name"
+    }
+    
+    public static func create(
+        humanId: String,
+        idDocumentType: IdentityDocumentType,
+        idDocumentIdentifier: String,
+        session: Session?,
+        callback: @escaping (Error?, Self?) -> Void
+    ) {
+        Request.make(
+            path: self.path,
+            payload: CreatePayload(
+                humanId: humanId,
+                idDocumentType: idDocumentType,
+                idDocumentIdentifier: idDocumentIdentifier
+            ),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            Request.decodeResponse(error, data, Self.self, callback)
+            return
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let humanId: String
+        let idDocumentType: IdentityDocumentType
+        let idDocumentIdentifier: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case humanId = "human_id"
+            case idDocumentType = "id_document_type"
+            case idDocumentIdentifier = "id_document_identifier"
+        }
     }
 }
