8fccd7007148e9a6b845f1b44528fd75f158d56b
Kayla <kayla.h@procuret.com>
2021-11-12 17:34:55 -0500

    added TrusteeType enum.

---
+++ Sources/ProcuretAPI/Entity/TrusteeType.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kayla Hoyet on 11/12/21.
+//
+
+import Foundation
+
+public enum TrusteeType: Int {
+    case trusteeIndividual = 1
+    case trusteeIncorporated = 2
+}
