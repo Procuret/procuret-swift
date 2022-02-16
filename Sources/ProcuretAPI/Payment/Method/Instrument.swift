001c4282fbc1cb3918293fc2a82130b0f7b017f8
Kayla <kayla.h@procuret.com>
2022-02-16 14:02:35 -0500

    Instrument and Processor structs added.

---
+++ Sources/ProcuretAPI/Payment/Method/Instrument.swift
@@ -0,0 +1,15 @@
+//
+//  Instrument.swift
+//  
+//
+//  Created by Kayla Hoyet on 2/16/22.
+//
+
+import Foundation
+import XCTest
+
+public enum Instrument: Int, Codable {
+    
+    case card = 1
+    case directDebit = 2
+}
---
+++ Sources/ProcuretAPI/Payment/Method/Processor.swift
@@ -0,0 +1,15 @@
+//
+//  Processor.swift
+//  
+//
+//  Created by Kayla Hoyet on 2/16/22.
+//
+
+import Foundation
+import XCTest
+
+public enum Processor: Int, Codable {
+    
+    case stripe = 1
+    case monoova = 2
+}
