84ddd22ab7f91786df74397504cdff00444a962a
Kayla <kayla.h@procuret.com>
2022-02-16 14:03:16 -0500

    PaymentMethod updated with retrieveMany function/ OrderBy enum.

--- Sources/ProcuretAPI/Payment/Method/Method.swift
+++ Sources/ProcuretAPI/Payment/Method/Method.swift
@@ -12,27 +12,39 @@ public struct PaymentMethod: Codable {
     internal static let path = "/payment/method"
     internal static let listPath = PaymentMethod.path + "/list"
     
-    internal static func retrieve(
-        publicId: String,
-        session: Session?,
-        callback: @escaping (Error?, PaymentMethod?) -> Void
-    ) {
-        Request.make(
-            path: self.path,
-            payload: RetrieveParameters(publicId: publicId),
-            session: session,
-            query: nil,
-            method: .GET
-        ) { error, data in
-            fatalError("Not implemented")
-        }
+    public enum OrderBy: String {
+        case created = "created"
     }
     
-    private struct RetrieveParameters: Codable {
-        let publicId: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case publicId = "public_id"
+    public static func retrieveMany(
+        limit: Int,
+        offset: Int,
+        order: Order,
+        orderBy: PaymentMethod.OrderBy,
+        entityId: String?,
+        authorityId: String?,
+        active: Bool?,
+        instrument: Instrument?,
+        processor: Processor?,
+        session: Session?,
+        callback: @escaping (Error?, Array<PaymentMethod>?) -> Void
+    ) {
+        Request.make(
+            path: PaymentMethod.listPath,
+            data: nil,
+            session: session,
+            query: QueryString(
+                targetsOnly: [
+                    UrlParameter(limit, key: "limit"),
+                    UrlParameter(offset, key: "offset"),
+                    UrlParameter(order.rawValue, key: "descending"),
+                    UrlParameter(orderBy.rawValue, key: "created")
+                ]
+            ),
+            method: .GET
+        ) { error, data in
+            Request.decodeResponse(error, data, Array<Self>.self, callback)
+            return
         }
     }
 }
