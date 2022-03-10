1ccb3eb049aa04f86c8d09722a1c3590b3a4b4e9
Kayla <kayla.h@procuret.com>
2022-03-10 09:28:14 -0500

    * updated PaymentSeries * successful test - PaymentSeries.retrieveMany()

--- Sources/ProcuretAPI/Payment/Series.swift
+++ Sources/ProcuretAPI/Payment/Series.swift
@@ -7,140 +7,63 @@
 
 import Foundation
 
-public struct PaymentSeries: Codable {
+public struct PaymentSeries: Codable, Identifiable {
     
     internal static let path = "/payment/series"
     internal static let listPath = PaymentSeries.path + "/list"
     
-    let publicId: String
-    let created: ProcuretTime
-    let creatingAgent: String
-    let paymentMethod: PaymentMethodHeadline
-    let customer: EntityHeadline
-    let supplier: EntityHeadline
-    let exchangeId: String
-    let amount: Amount
-    let sumPayments: String
-    let identifier: String
-    let disposition: Disposition
+    public let publicId: String
+    public let creatingAgent: Int
+    public let paymentMethod: PaymentMethodHeadline
+    public let customer: EntityHeadline
+    public let supplier: EntityHeadline
+    public let exchangeId: String
+    public let identifier: String
+    public let disposition: Disposition
+    public let id = UUID()
     
     public enum CodingKeys: String, CodingKey {
         case publicId = "public_id"
-        case created
         case creatingAgent = "creating_agent"
         case paymentMethod = "payment_method"
         case customer
         case supplier
         case exchangeId = "exchange_id"
-        case amount
-        case sumPayments = "sum_payments"
         case identifier
         case disposition
     }
     
-    public static func create(
-        paymentMethod: PaymentMethod,
-        setupId: String,
-        months: Int,
+    public enum OrderBy: String {
+        case created = "created"
+    }
+    
+    public static func retrieveMany(
+        limit: Int,
+        offset: Int,
+        order: Order,
+        orderBy: PaymentSeries.OrderBy,
+        textFragment: String?,
+        businessId: String?,
+        methodId: String?,
         session: Session?,
-        callback: @escaping (Error?) -> Void
+        callback: @escaping (Error?, Array<Self>?) -> Void
     ) {
         Request.make(
-            path: self.path,
-            payload: CreatePayload(
-                paymentMethod: paymentMethod,
-                setupId: setupId,
-                months: months
+            path: Self.listPath,
+            data: nil,
+            session: session,
+            query: QueryString(
+                targetsOnly: [
+                    UrlParameter(limit, key: "limit"),
+                    UrlParameter(offset, key: "offset"),
+                    UrlParameter(order.rawValue, key: "descending"),
+                    UrlParameter(orderBy.rawValue, key: "created")
+                ]
             ),
-            session: session,
-            query: nil,
-            method: .POST
-        ) { error, data in
-            fatalError("Not implemented")
-        }
-    }
-    
-    public static func createWithCommitment(
-        commitmentId: String,
-        session: Session?,
-        callback: @escaping (Error?, Self?) -> Void
-    ) {
-        Request.make(
-            path: self.path,
-            payload: CreateCommitmentPayload(commitmentId: commitmentId),
-            session: session,
-            query: nil,
-            method: .POST
-        ) { error, data in
-            fatalError("Not implemented")
-        }
-    }
-    
-    public static func retrieve(
-        publicId: String,
-        session: Session?,
-        callback: @escaping (Error?, Self?) -> Void
-    ) {
-        Request.make(
-            path: self.path,
-            payload: RetrieveParameters(publicId: publicId),
-            session: session,
-            query: nil,
             method: .GET
         ) { error, data in
-            fatalError("Not implemented")
-        }
-    }
-    
-    public static func retrieveByCommitment(
-        commitmentId: String,
-        session: Session?,
-        callback: @escaping (Error?, Self?) -> Void
-    ) {
-        Request.make(
-            path: self.path,
-            payload: RetrieveCommitment(commitmentId: commitmentId),
-            session: session,
-            query: nil,
-            method: .GET
-        ) { error, data in
-            fatalError("Not implemented")
-        }
-    }
-    
-    private struct CreatePayload: Codable {
-        let paymentMethod: PaymentMethod
-        let setupId: String
-        let months: Int
-        
-        private enum CodingKeys: String, CodingKey {
-            case paymentMethod = "payment_method_id"
-            case setupId = "setup_id"
-            case months = "periods"
-        }
-    }
-    
-    private struct CreateCommitmentPayload: Codable {
-        let commitmentId: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case commitmentId = "commitment_id"
-        }
-    }
-    
-    private struct RetrieveParameters: Codable {
-        let publicId: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case publicId = "public_id"
-        }
-    }
-    
-    private struct RetrieveCommitment: Codable {
-        let commitmentId: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case commitmentId = "commitment_id"
+            Request.decodeResponse(error, data, Array<Self>.self, callback)
+            return
         }
     }
 }
---
+++ Tests/ProcuretAPITests/PaymentSeriesTest.swift
@@ -0,0 +1,50 @@
+//
+//  PaymentSeriesTest.swift
+//  
+//
+//  Created by Kayla Hoyet on 3/9/22.
+//
+
+import XCTest
+@testable import ProcuretAPI
+
+class PaymentSeriesTest: XCTestCase {
+    
+    func provideTestSession() -> Session {
+        
+        do {
+            return try Session.fromEnvironmentVariables()
+        } catch {
+            fatalError("Unable to initalise test session: \(error)")
+        }
+    }
+    
+    func testRetrieveManyPaymentSeries() {
+        
+        let expectation = XCTestExpectation(description: "retrieveMany Payment Series")
+        
+        func recieveManyPaymentSeries(error: Error?, paymentSeries: Array<PaymentSeries>?) {
+            
+            XCTAssertNil(error, "An error occurred.")
+            XCTAssertNotNil(paymentSeries, "Payment Series is nil.")
+            
+            expectation.fulfill()
+            
+            return
+        }
+        
+        PaymentSeries.retrieveMany(
+            limit: 5,
+            offset: 1,
+            order: Order.descending,
+            orderBy: PaymentSeries.OrderBy.created,
+            textFragment: nil,
+            businessId: nil,
+            methodId: nil,
+            session: provideTestSession(),
+            callback: recieveManyPaymentSeries
+        )
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+}
