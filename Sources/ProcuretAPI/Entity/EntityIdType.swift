6a69bae7f2ca0261541634f47890f565fcb96826
Hugh Jeremy <hugh.jeremy@gmail.com>
2022-03-26 16:47:40 +1100

    add `EntityIdType.name` property

--- Sources/ProcuretAPI/Entity/EntityIdType.swift
+++ Sources/ProcuretAPI/Entity/EntityIdType.swift
@@ -3,16 +3,32 @@
 //  
 //
 //  Created by Kayla Hoyet on 7/22/21.
+//  Modified by Hugh Jeremy on 26 Mar 2022
 //
 
 import Foundation
 
 
-public enum EntityIdType: Int, Codable, CaseIterable {
+public enum EntityIdType: Int, Codable, CaseIterable, Identifiable {
 
     case abn = 1
     case acn = 2
     case nzbn = 3
     case nzcn = 4
     
+    public var name: String { get {
+        switch self {
+        case .abn:
+            return "Australian Business Number"
+        case .acn:
+            return "Australian Company Number"
+        case .nzbn:
+            return "New Zealand Business Number"
+        case .nzcn:
+            return "New Zealand Company Number"
+        }
+    } }
+    
+    public var id: Int { get { return self.rawValue } }
+
 }
