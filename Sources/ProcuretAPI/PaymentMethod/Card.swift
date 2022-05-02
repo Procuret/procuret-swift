85e7fe84ff604193cc849a87d65064afa3c1e75f
Kayla <kayla.h@procuret.com>
2022-05-02 13:33:20 -0400

    card creation method added + testing - successful

--- Sources/ProcuretAPI/PaymentMethod/Card.swift
+++ Sources/ProcuretAPI/PaymentMethod/Card.swift
@@ -26,5 +26,55 @@ public struct Card: UnderpinnedByMethodKernel {
         case kernel
         case details = "card_details"
     }
-
+    
+    public static func createFromPlainDetails(
+        entityId: Int?,
+        authorityAgentId: Int,
+        cardNumber: String,
+        expiryMonth: String,
+        expiryYear: String,
+        cvc: String,
+        postalCode: String,
+        session: Session?,
+        callback: @escaping (Error?, Card?) -> Void
+    ) {
+        Request.make(
+            path: "/payment/method/card/plain",
+            payload: CreatePayload(
+                entityId: entityId,
+                authorityAgentId: authorityAgentId,
+                cardNumber: cardNumber,
+                expiryMonth: expiryMonth,
+                expiryYear: expiryYear,
+                cvc: cvc,
+                postalCode: postalCode
+            ),
+            session: session,
+            query: nil,
+            method: .POST
+        ) { error, data in
+            Request.decodeResponse(error, data, Self.self, callback)
+            return
+        }
+    }
+    
+    private struct CreatePayload: Codable {
+        let entityId: Int?
+        let authorityAgentId: Int
+        let cardNumber: String
+        let expiryMonth: String
+        let expiryYear: String
+        let cvc: String
+        let postalCode: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case entityId = "entity_id"
+            case authorityAgentId = "authority_agent_id"
+            case cardNumber = "card_number"
+            case expiryMonth = "expiry_month"
+            case expiryYear = "expiry_year"
+            case cvc = "cvc"
+            case postalCode = "postcode"
+        }
+    }
 }
---
+++ Tests/ProcuretAPITests/CardTests.swift
@@ -0,0 +1,42 @@
+//
+//  CardTests.swift
+//  
+//
+//  Created by Kayla Hoyet on 5/2/22.
+//
+
+import XCTest
+@testable import ProcuretAPI
+
+class CardTests: XCTestCase {
+    
+    func testCreateCard() {
+        
+        let expectation = XCTestExpectation(
+            description: "create Credit Card"
+        )
+        
+        func createCreditCard(error: Error?, card: Card?) {
+            XCTAssertNil(error, "An error occurred.")
+            XCTAssertNotNil(card, "Credit card is nil.")
+            
+            expectation.fulfill()
+            return
+        }
+        
+        Card.createFromPlainDetails(
+            entityId: 7063459056099762,
+            authorityAgentId: 100,
+            cardNumber: "4242424242424242",
+            expiryMonth: "12",
+            expiryYear: "25",
+            cvc: "424",
+            postalCode: "42424",
+            session: Session.forceFromEnvironmentVariables(),
+            callback: createCreditCard
+        )
+        
+        wait(for: [expectation], timeout: 5.0)
+        return
+    }
+}
