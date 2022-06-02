c747fe78e64d4d61ea875beaaa46eccc04cabf34
Kayla <kayla.h@procuret.com>
2022-06-02 15:46:46 -0400

    Document testing Update to version 0.0.54

--- Tests/ProcuretAPITests/DocumentTests.swift
+++ Tests/ProcuretAPITests/DocumentTests.swift
@@ -26,5 +26,37 @@ class DocumentTests: XCTestCase {
         return
         
     }
+    
+    func testTOSRetrieval() {
+        
+        let expectation = XCTestExpectation()
+        
+        TOS.retrieveLatest { error, agreement in
+            XCTAssertNil(error)
+            XCTAssertNotNil(agreement)
+            expectation.fulfill()
+            return
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
+        return
+        
+    }
+    
+    func testPrivacyPolicyRetrieval() {
+        
+        let expectation = XCTestExpectation()
+        
+        PrivacyPolicy.retrieveLatest { error, agreement in
+            XCTAssertNil(error)
+            XCTAssertNotNil(agreement)
+            expectation.fulfill()
+            return
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
+        return
+        
+    }
 
 }
