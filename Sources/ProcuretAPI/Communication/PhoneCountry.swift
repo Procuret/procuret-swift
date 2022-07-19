c55f76ae2812af4d35689f88f56db474257376fa
hwjeremy <hugh.jeremy@gmail.com>
2022-07-19 15:12:07 +1000

    add `PhoneCountryCode` enum

---
+++ Sources/ProcuretAPI/Communication/PhoneCountry.swift
@@ -0,0 +1,18 @@
+//
+//  PhoneCountry.swift
+//  
+//
+//  Created by Hugh on 19/7/2022.
+//
+
+import Foundation
+
+public enum PhoneCountryCode: String {
+    
+    case Australia = "+61"
+    case NewZealand = "+64"
+    case UnitedStates = "+1"
+    case Singapore = "+65"
+    case Other = ""
+
+}
