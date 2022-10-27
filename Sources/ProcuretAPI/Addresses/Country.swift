42b8923e11c619f856d580702a4547f0f301f751
Kayla <kayla.h@procuret.com>
2022-10-27 19:54:38 -0400

    new and improved Country and Region structs.

--- Sources/ProcuretAPI/Addresses/Country.swift
+++ Sources/ProcuretAPI/Addresses/Country.swift
@@ -1,28 +1,63 @@
 //
 //  Country.swift
-//  
+//
 //
 //  Created by Kayla Hoyet on 7/5/21
+//  Modified by hugh@blinkybeach.com
 //
 
 import Foundation
 
-
-public struct Country: Codable {
-    
-    public enum countryId: Int {
-        case countryAustralia = 1
-        case countryNewZealand = 2
-    }
-    
+public struct Country: Codable, Identifiable, Hashable {
+    public let countryId: Int
     public let name: String
     public let iso3166a2: String
     public let iso3166a3: String
     
+    public var id: Int { get { return self.countryId } }
+    public var enumeratedId: Self.Id { get {
+        return Self.Id(rawValue: self.countryId)!
+    } }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self.countryId)
+    }
+    
     private enum CodingKeys: String, CodingKey {
+        case countryId = "country_id"
         case name
         case iso3166a2 = "iso_3166_a2"
         case iso3166a3 = "iso_3166_a3"
     }
-
+    
+    private init(
+        _ countryId: Self.Id,
+        _ name: String,
+        _ iso3166a2: String,
+        _ iso3166a3: String
+    ) {
+        self.countryId = countryId.rawValue
+        self.name = name
+        self.iso3166a2 = iso3166a2
+        self.iso3166a3 = iso3166a3
+        return
+    }
+    
+    public enum Id: Int {
+    
+        case Australia = 1
+        case NewZealand = 2
+    }
+    
+    public static let all = [
+        Country(.Australia, "Australia", "AU", "AUS"),
+        Country(.NewZealand, "New Zealand", "NZ", "NZL")
+    ]
+    
+    public static func withId(_ id: Self.Id) -> Country {
+        for country in Self.all {
+            if country.enumeratedId == id { return country }
+        }
+        fatalError("Unknown Country ID \(id.rawValue)")
+    }
 }
--- Sources/ProcuretAPI/Addresses/Region.swift
+++ Sources/ProcuretAPI/Addresses/Region.swift
@@ -1,16 +1,42 @@
 //
 //  Region.swift
-//  
+//
 //
 //  Created by Hugh Jeremy on 11/6/21.
 //
-
 import Foundation
 
-
-public struct Region: Codable {
+public struct Region: Codable, Identifiable, Hashable {
+    
+    public let indexId: Int
+    public let name: String
+    public let abbreviation: String
+    public let countryId: Int
+    
+    public var id: Int { get { return self.indexId; } }
+    
+    private enum CodingKeys: String, CodingKey {
+        case indexId = "indexid"
+        case name
+        case abbreviation = "abbreviation"
+        case countryId = "country_id"
+    }
+    
+    private init(
+        _ id: Self.AustralianRegionId,
+        _ name: String,
+        _ abbreviation: String,
+        _ countryId: Country.Id
+    ) {
+        self.indexId = id.rawValue
+        self.name = name
+        self.abbreviation = abbreviation
+        self.countryId = countryId.rawValue
+        return
+    }
     
     public enum AustralianRegionId: Int {
+        
         case NSW = 1
         case VIC = 2
         case QLD = 3
@@ -21,16 +47,35 @@ public struct Region: Codable {
         case ACT = 8
     }
     
-    public let indexId: Int
-    public let name: String
-    public let abbreviation: String
-    public let countryId: Int
+    public static let australianRegions = [
+        Region(.NSW, "New South Wales", "NSW", .Australia),
+        Region(.VIC, "Victoria", "VIC", .Australia),
+        Region(.QLD, "Queensland", "QLD", .Australia),
+        Region(.SA, "South Australia", "SA", .Australia),
+        Region(.WA, "Western Australia", "WA", .Australia),
+        Region(.TAS, "Tasmania", "TAS", .Australia),
+        Region(.NT, "Northern Territory", "NT", .Australia),
+        Region(.ACT, "Australian Capital Territory", "ACT", .Australia)
+    ]
     
-    private enum CodingKeys: String, CodingKey {
-        case indexId = "indexid"
-        case name
-        case abbreviation = "abbreviation"
-        case countryId = "country_id"
+    public func hash(into hasher: inout Hasher) {
+        return hasher.combine(self.indexId)
     }
+    
+    public static func regionsIn(country: Country) -> Array<Region> {
+        switch country.enumeratedId {
+        case .Australia:
+            return Self.australianRegions
+        case .NewZealand:
+            return []
+        }
+    }
+    
+    public static func withId(_ id: Self.AustralianRegionId) -> Region {
+        for region in Self.australianRegions {
+            if region.indexId == id.rawValue { return region }
+        }
+        fatalError("Unknown Region ID \(id.rawValue)")
+    }
+    
 }
-
