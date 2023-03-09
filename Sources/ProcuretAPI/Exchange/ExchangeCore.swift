01ca99fc28c62ed1325b0f89c0466efee58bbca6
Kayla <kayla.h@procuret.com>
2023-03-09 18:49:18 -0500

    added ExchangeCore and PendingSeries structs.

---
+++ Sources/ProcuretAPI/Exchange/ExchangeCore.swift
@@ -0,0 +1,25 @@
+//
+//  ExchangeCore.swift
+//  
+//
+//  Created by Kayla Hoyet on 3/9/23.
+//
+
+import Foundation
+
+public struct ExchangeCore: Codable {
+    
+    public let publicId: String
+    public let created: ProcuretDate
+    public let supplier: EntityHeadline
+    public let amount: Amount
+    public let description: String
+    
+    public enum CodingKeys: String, CodingKey {
+        case publicId = "public_id"
+        case created
+        case supplier
+        case amount
+        case description
+    }
+}
---
+++ Sources/ProcuretAPI/Payment/PendingSeries.swift
@@ -0,0 +1,68 @@
+//
+//  PendingSeries.swift
+//  
+//
+//  Created by Kayla Hoyet on 3/9/23.
+//
+
+import Foundation
+
+public struct PendingSeries: Codable {
+    
+    internal static let path = "/payment-series/pending"
+    internal static let listPath = PendingSeries.path + "/list"
+    
+    public let exchangeCore: ExchangeCore
+    public let commitmentId: String
+    public let awaitingIdentityAssessment: Bool?
+    public let awaitingCreditAssessment: Bool?
+    public let disposition: Disposition
+    
+    public enum CodingKeys: String, CodingKey {
+        case exchangeCore = "exchange_core"
+        case commitmentId = "commitment_id"
+        case awaitingIdentityAssessment = "awaiting_identity_assessment"
+        case awaitingCreditAssessment = "awaiting_credit_assessment"
+        case disposition
+    }
+    
+    public static func retrieveMany(
+        
+        limit: Int,
+        offset: Int,
+        supplierId: String?,
+        businessId: String?,
+        humanId: String?,
+        awaitingIdentity: Bool?,
+        awaitingCredit: Bool?,
+        endpoint: ApiEndpoint = ApiEndpoint.live,
+        session: SessionRepresentative,
+        callback: @escaping (Error?, Array<Self>?) -> Void
+    ) {
+        typealias UP = UrlParameter
+        
+        Request.make(
+            path: self.listPath,
+            data: nil,
+            session: session,
+            query: QueryString(
+                targetsOnly: [
+                    UP(limit, key: "limit"),
+                    UP(offset, key: "offset"),
+                    UP.optionally(supplierId, key: "supplier_id"),
+                    UP.optionally(businessId, key: "business_id"),
+                    UP.optionally(humanId, key: "human_id"),
+                    UP.optionally(awaitingIdentity,
+                                  key: "awaiting_identity_assessment"),
+                    UP.optionally(awaitingCredit,
+                                  key: "awaiting_credit_assessment")
+                ].compactMap { $0 }
+            ),
+            method: .GET,
+            endpoint: endpoint
+        ) { error, data in
+            Request.decodeResponse(error, data, Array<Self>.self, callback)
+            return
+        }
+    }
+}
