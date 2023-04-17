99276f2c2e054da81c62363ae10fae59fa82c67f
hwjeremy <hugh.jeremy@gmail.com>
2023-04-17 11:51:05 +1000

    add `InstalmentPlanApplication`, remove `SellStage`

---
+++ Sources/ProcuretAPI/InstalmentPlan/Application.swift
@@ -0,0 +1,60 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hugh on 17/4/2023.
+//
+
+import Foundation
+
+
+public struct InstalmentPlanApplication {
+    
+    internal static let path = "/magic-invoice"
+    
+    static func create(
+        authenticatedBy session: Session,
+        supplier: Supplier,
+        business: Business,
+        invoiceIdentifier: String,
+        invoiceAmount: Amount,
+        months: Int,
+        method: PaymentMethod? = nil,
+        linkId: String? = nil,
+        at endpoint: ApiEndpoint = .live,
+        then callback: @escaping (Error?) -> Void
+    ) -> Void {
+        
+        Request.make(
+            path: Self.path,
+            payload: CreatePayload(
+                supplier_id: supplier.entity.publicId,
+                business_id: business.entity.publicId,
+                invoice_identifier: invoiceIdentifier,
+                invoice_amount: invoiceAmount,
+                periods: months,
+                link_id: linkId
+            ),
+            session: session,
+            query: nil,
+            method: .POST,
+            endpoint: endpoint,
+            then: { error, _ in callback(error); return }
+        )
+
+        return
+
+    }
+    
+    private struct CreatePayload: Encodable {
+
+        let supplier_id: Int
+        let business_id: Int
+        let invoice_identifier: String
+        let invoice_amount: Amount
+        let periods: Int
+        let link_id: String?
+
+    }
+    
+}
