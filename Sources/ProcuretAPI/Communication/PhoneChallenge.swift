f6b880f6b541d494aaac41f3ebeb4b3c602ae799
hwjeremy <hugh.jeremy@gmail.com>
2023-05-03 16:53:54 +1000

    add `PhoneChallenge`

---
+++ Sources/ProcuretAPI/Communication/PhoneChallenge.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hugh on 3/5/2023.
+//
+
+import Foundation
+
+public struct PhoneChallenge {
+    
+    private static let path = "/phone-number/challenge"
+    
+    public static func create(
+        authenticatedBy session: SessionRepresentative,
+        for number: PhoneNumber,
+        at endpoint: ApiEndpoint = .live,
+        then callback: @escaping (Error?) -> Void
+    ) {
+        
+        Request.make(
+            path: Self.path,
+            data: nil,
+            session: session,
+            query: QueryString(
+                [UrlParameter(number.phoneId, key: "phone_id")]
+            ),
+            method: .POST,
+            endpoint: endpoint
+        ) { error, _ in callback(error); return }
+        
+        return
+
+    }
+    
+}
+
