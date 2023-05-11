9e3c0b26988c1e0ea538132b2d4f83ab090840d6
hwjeremy <hugh.jeremy@gmail.com>
2023-05-11 12:09:26 +1000

    flesh out `IdentityDocumentType`

--- Sources/ProcuretAPI/Human/IdentityDocumentType.swift
+++ Sources/ProcuretAPI/Human/IdentityDocumentType.swift
@@ -7,6 +7,40 @@
 
 import Foundation
 
-public enum IdentityDocumentType: Int {
-    case ausDriversLicense = 1
+public enum IdentityDocumentType: Int, Codable, Identifiable {
+
+    case australianDriversLicense = 1
+    case newZealandDriversLicense = 2
+    
+    public var id: Int { get { return self.rawValue } }
+
+    public var name: String { get {
+        switch self {
+        case .australianDriversLicense:
+            return "Australian Drivers License"
+        case .newZealandDriversLicense:
+            return "New Zealand Drivers License"
+        }
+    } }
+    
+    public var associatedCountry: Country { get {
+        switch self {
+        case .australianDriversLicense:
+            return Country.australia
+        case .newZealandDriversLicense:
+            return Country.newZealand
+        }
+    } }
+    
+    public static func associatedWith(country: Country) -> Self? {
+        switch country {
+        case Country.australia:
+            return .australianDriversLicense
+        case Country.newZealand:
+            return .newZealandDriversLicense
+        default:
+            return nil
+        }
+    }
+    
 }
